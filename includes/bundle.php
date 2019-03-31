2e62caa4f46aa30e81f5bf66802d41158aabbe7e
madan <hamood0.2009@gmail.com>
2019-03-31 19:55:23 +0300

    -added login/Register modal -fixed the javascript imports

--- includes/bundle.php
+++ includes/bundle.php
@@ -7,9 +7,13 @@
  */
 
 
-ini_set('show_errors', 'On');
-ini_set('display_errors', 1);
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+$debug = true;
+if($debug){
+    ini_set('show_errors', 'On');
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+}
+
 
 function __autoload($className){
     include_once  $className.'.php';
